f4e3a391ade912686a88b20de1d3402e292662eb
GIFT <giftabumere247@gmail.com>
2023-03-03 00:09:12 +0100

    cheeeeeeee

--- gwitter/views.py
+++ gwitter/views.py
@@ -11,7 +11,21 @@ def profile_list(request):
 
 def profile(request, username):
     profile = Profile.objects.get(user=User.objects.get(username=username))
+
+    if request.method == 'POST':
+        current_user_profile = request.user.profile
+        data = request.POST
+        action = data.get('follow')
+
+        if action == 'follow':
+            current_user_profile.follows.add(profile)
+        elif action == 'unfollow':
+            current_user_profile.follows.remove(profile)
+        
+        current_user_profile.save()
+
     user_follows = profile in request.user.profile.follows.all()
+    
     return render(request, 'gwitter/profile.html', 
                   {'profile':profile, 'followers':len(profile.followed_by.all()), 
                    'follows':len(profile.follows.all()), 'user_follows':user_follows})
